6f5d99c9991247b50865a0eb774b9e00c2c16b9a
ahsancse15 <90628947+ahsancse15@users.noreply.github.com>
2023-06-22 16:50:14 +0600

    Add files via upload

---
+++ AppUser/AppUser/src/main/java/com/example/AppUser/registration/RegistrationRequest.java
@@ -0,0 +1,18 @@
+package com.example.AppUser.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+}
